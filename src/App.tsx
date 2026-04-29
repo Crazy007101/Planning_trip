afb1a2f5a40145788581093e7974d1cec7396035
Eduard <eduard.agishev.2005@mail.ru>
2026-04-29 11:02:43 +0300

    feat: add scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import CreateJourney from './pages/CreateJourney';
 import Home from './pages/Home.tsx';
 import TripsList from './pages/TripsList.tsx';
+import ScrollToTop from './components/ScrollToTop.tsx';
 
 const theme = createTheme({
   palette: {
@@ -14,6 +15,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<CreateJourney />} />
